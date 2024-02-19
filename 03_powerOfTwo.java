9eb24516e22966aee60ed0f9ab24b3cedddfbc5a
ayushsingh953 <ayushsingh35323@gmail.com>
2024-02-19 23:36:43 +0530

    WA for two test cases

---
+++ 03_powerOfTwo.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/power-of-two/?envType=daily-question&envId=2024-02-19
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        return Math.floor(Math.log(n)/Math.log(2)) == Math.ceil(Math.log(n)/Math.log(2));
+        
+    }
+}
