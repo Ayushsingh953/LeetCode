1aabb08b91ed41c608f5d42c27c8ec73fb6ab815
ayushsingh953 <ayushsingh35323@gmail.com>
2024-02-20 16:16:53 +0530

    Sol-2

--- 05_shuffleArray.java
+++ 05_shuffleArray.java
@@ -20,3 +20,20 @@ class Solution {
         return res;
     }
 }
+
+
+// Without using extra space
+// Store the pair of numbers
+class Solution2 {
+    public int[] shuffle(int[] nums, int n) {
+        for(int i=n;i<2*n;i++){
+            nums[i]=(nums[i]*1024)+nums[i-n];
+        }
+        int k=0;
+        for(int i=n;i<2*n;i++){
+            nums[k++]=nums[i]%1024;
+            nums[k++]=nums[i]/1024;
+        }
+        return nums;
+    }
+}
