b8b0b96de17d84be93dbea7c66c5805d52283b1f
ayushsingh953 <ayushsingh35323@gmail.com>
2024-02-20 16:30:18 +0530

    Nested loop

--- 06_WordsContainingChar.java
+++ 06_WordsContainingChar.java
@@ -14,3 +14,17 @@ class Solution {
     }
 }
 
+class Solutions {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> s=new ArrayList<Integer>();
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<words[i].length();j++){
+                if(words[i].charAt(j)==x){
+                    s.add(i);
+                    break;
+                }
+            }
+        }
+        return s;
+    }
+}
