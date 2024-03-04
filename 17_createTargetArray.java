0a59e780f37bb1b59f096f3b4ad28bf0329beda9
ayushsingh953 <ayushsingh35323@gmail.com>
2024-03-04 15:28:50 +0530

    1389. Create Target Array in the Given Order

---
+++ 17_createTargetArray.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target=new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            int ind=index[i];
+            if(ind<i){
+                for(int j=i;j>ind;j--){
+                    target[j]=target[j-1];
+                }
+            }
+            target[ind]=nums[i];
+        }
+        return target;
+    }
+}
