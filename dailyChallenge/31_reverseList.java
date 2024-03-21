94c71302b13f625fe761be58940d07c335c197b5
ayushsingh953 <ayushsingh35323@gmail.com>
2024-03-21 14:55:35 +0530

    day 32:206. Reverse Linked List

---
+++ dailyChallenge/31_reverseList.java
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+//using Stack and loop
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null){
+            return head;
+        }
+       Stack<ListNode> s=new Stack<>();
+       ListNode temp=head;
+       while(temp!=null){
+        s.push(temp);
+        temp=temp.next;
+       }
+       temp=s.pop();
+       head=temp;
+       while(!s.empty()){
+        temp.next=s.pop();
+        temp=temp.next;
+       }
+       temp.next=null;
+       return head;
+    }
+}
+
+
+//Using loop only
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode cur=head;
+        ListNode next=null;
+
+        while(cur!=null){
+            next=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=next;
+        }
+        head=prev;
+        return head;
+    }
