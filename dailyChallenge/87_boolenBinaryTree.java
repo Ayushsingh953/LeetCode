ca2996a27951ac29ff293e3731f6cd9698555d8e
ayushsingh953 <ayushsingh35323@gmail.com>
2024-05-16 23:11:27 +0530

    Day 88: 2331. Evaluate Boolean Binary Tree

---
+++ dailyChallenge/87_boolenBinaryTree.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/evaluate-boolean-binary-tree/?envType=daily-question&envId=2024-05-16
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.val == 0) return false;
+        if(root.val == 1) return true;
+        boolean l = evaluateTree(root.left);
+        boolean r = evaluateTree(root.right);
+        return root.val==2 ? l|r : l&r;
+    }
+}
