6e33b9f9391b303b1ebc9bd0d74ec3812ea31c79
ayushsingh953 <ayushsingh35323@gmail.com>
2024-05-26 22:14:30 +0530

    Day 95: 2597. The Number of Beautiful Subsets

---
+++ dailyChallenge/94_beautifulSubsets.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/the-number-of-beautiful-subsets/
+
+class Solution {
+    private HashMap<Integer, Integer> freq;
+
+    private int beautifulSubsets(int[] nums, int k, int i) {
+        if (i == nums.length) { // base case
+            return 1;
+        }
+        int result = beautifulSubsets(nums, k, i + 1); // nums[i] not taken
+        if (!freq.containsKey(nums[i] - k)) { // check if we can take nums[i]
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+            result += beautifulSubsets(nums, k, i + 1); // nums[i] taken
+            freq.put(nums[i], freq.get(nums[i]) - 1);
+            if (freq.get(nums[i]) == 0) {
+                freq.remove(nums[i]);
+            }
+        }
+        return result;
+    }
+
+    public int beautifulSubsets(int[] nums, int k) {
+        freq = new HashMap<Integer, Integer>();
+        Arrays.sort(nums);
+        return beautifulSubsets(nums, k, 0) - 1; // -1 for empty subset
+    }
+}
