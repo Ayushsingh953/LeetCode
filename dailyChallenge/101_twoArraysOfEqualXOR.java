fb235eb03e29fd8f14f121e401c350d159127b02
ayushsingh953 <ayushsingh35323@gmail.com>
2024-05-30 21:56:00 +0530

    1442. Count Triplets That Can Form Two Arrays of Equal XOR

---
+++ dailyChallenge/101_twoArraysOfEqualXOR.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+
+class Solution {
+    public int countTriplets(int[] arr) {
+       int ans = 0;
+       int length = arr.length;
+       for (int i = 0; i < length; i++) {
+           int xor = arr[i];
+           for (int j = i + 1; j < length; j++) {
+               xor ^= arr[j];
+               if (xor == 0) {
+                   ans += (j - i);
+               }
+           }
+       }
+       return ans;
+   }
+}
