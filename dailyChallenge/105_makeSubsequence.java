950dc0eef4712fc41d9ac9d691a4d67d97680742
ayushsingh953 <ayushsingh35323@gmail.com>
2024-06-03 15:27:00 +0530

    2486. Append Characters to String to Make Subsequence

---
+++ dailyChallenge/105_makeSubsequence.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
+
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int sIndex = 0, tIndex = 0;
+        int sLength = s.length(), tLength = t.length();
+        
+        while (sIndex < sLength && tIndex < tLength) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                tIndex++;
+            }
+            sIndex++;
+        }
+        
+        return tLength - tIndex;
+        
+    }
+}
