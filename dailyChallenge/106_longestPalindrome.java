3d36c0c32b2b058ee06de7fff0826ed5e43e3eb6
ayushsingh953 <ayushsingh35323@gmail.com>
2024-06-04 15:48:00 +0530

    409. Longest Palindrome

---
+++ dailyChallenge/106_longestPalindrome.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-palindrome/
+
+class Solution {
+
+    public int longestPalindrome(String s) {
+        Set<Character> characterSet = new HashSet<>();
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            if (characterSet.contains(c)) {
+                characterSet.remove(c);
+                res += 2;
+            } else {
+
+                characterSet.add(c);
+            }
+        }
+        if (!characterSet.isEmpty()) res++;
+
+        return res;
+    }
+}
