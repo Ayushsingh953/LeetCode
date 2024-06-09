f78f718a35da560c16aa934d9442d4d8bf559c14
ayushsingh953 <ayushsingh35323@gmail.com>
2024-06-09 11:58:00 +0530

    974. Subarray Sums Divisible by K

---
+++ dailyChallenge/111_subarraySumDivisibleByK.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/subarray-sums-divisible-by-k/
+
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int prefixMod = 0, result = 0;
+
+        int[] modGroups = new int[k];
+        modGroups[0] = 1;
+
+        for (int num: nums) {
+          
+            prefixMod = (prefixMod + num % k + k) % k;
+
+            result += modGroups[prefixMod];
+            modGroups[prefixMod]++;
+        }
+
+        return result;
+    }
+}
