8216351b24bc864bf3cec787484e8026b0b8630b
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-25 11:47:36 +0530

    872. Leaf-Similar Trees

---
+++ 55_leafSimilarTrees.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/leaf-similar-trees/?envType=study-plan-v2&envId=leetcode-75
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaf1 = new ArrayList<>();
+        List<Integer> leaf2 = new ArrayList<>();
+
+        dfs(root1,leaf1);
+        dfs(root2,leaf2);
+        return leaf1.equals(leaf2);
+    }
+    public void dfs(TreeNode root,List<Integer> leaf){
+        if(root == null) return;
+        if(root.left==null && root.right==null){
+            leaf.add(root.val);
+            return;
+        }
+        dfs(root.left,leaf);
+        dfs(root.right,leaf);
+    }
+}
