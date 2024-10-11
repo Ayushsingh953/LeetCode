31e8748bfc5bc6beb3f10f1ab5f9352edc71ba25
ayushsingh953 <ayushsingh35323@gmail.com>
2024-10-11 21:39:03 +0530

    1942. The Number of the Smallest Unoccupied Chair

---
+++ dailyChallenge/235_unoccupiedChair.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair
+
+class Solution {
+
+    public int smallestChair(int[][] times, int targetFriend) {
+        int[] targetTime = times[targetFriend];
+        Arrays.sort(times, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int n = times.length;
+        int[] chairTime = new int[n];
+
+        for (int[] time : times) {
+            for (int i = 0; i < n; i++) {
+                if (chairTime[i] <= time[0]) {
+                    chairTime[i] = time[1];
+                    if (Arrays.equals(time, targetTime)) return i;
+                    break;
+                }
+            }
+        }
+        return 0;
+    }
+}
