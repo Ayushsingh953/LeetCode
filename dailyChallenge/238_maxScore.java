5e0cde457a473febfb9a349ca6e9eb3c02f062dc
ayushsingh953 <ayushsingh35323@gmail.com>
2024-10-14 20:48:41 +0530

    2530. Maximal Score After Applying K Operations

---
+++ dailyChallenge/238_maxScore.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/maximal-score-after-applying-k-operations/
+
+class Solution {
+
+    public long maxKelements(int[] nums, int k) {
+        long ans = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            Collections.reverseOrder()
+        );
+
+        for (int i : nums) {
+            pq.add(i);
+        }
+
+        while (k-- > 0) {
+            int maxElement = pq.poll();
+            ans += maxElement;
+            pq.add((int) Math.ceil(maxElement / 3.0));
+        }
+        return ans;
+    }
+}
