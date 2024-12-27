ce058b99d350fc630a4bd286eab337887f0e1522
ayushsingh953 <ayushsingh35323@gmail.com>
2024-12-27 11:53:19 +0530

    1014. Best Sightseeing Pair

---
+++ dailyChallenge/312_sightSeeingPair.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/best-sightseeing-pair/
+
+class Solution {
+
+    public int maxScoreSightseeingPair(int[] values) {
+        int n = values.length;
+
+        int maxLeftScore = values[0];
+
+        int maxScore = 0;
+
+        for (int i = 1; i < n; i++) {
+            int currentRightScore = values[i] - i;
+            maxScore = Math.max(maxScore, maxLeftScore + currentRightScore);
+
+            int currentLeftScore = values[i] + i;
+            maxLeftScore = Math.max(maxLeftScore, currentLeftScore);
+        }
+
+        return maxScore;
+    }
+}
